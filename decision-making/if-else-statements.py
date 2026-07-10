35633f62288f7329961d2ebec7064b176c4c0f9f
waseem123 <waseem_6957@yahoo.com>
2026-07-10 17:35:32 +0530

    if-else

---
+++ decision-making/if-else-statements.py
@@ -0,0 +1,6 @@
+n = int(input('ENTER A NUMBER - '))
+
+if n % 2 == 0:
+    print(n,'IS AN EVEN NUMBER')
+else:
+    print(n,'IS AN ODD NUMBER')
---
+++ decision-making/if-statements.py
@@ -0,0 +1,8 @@
+n = int(input('ENTER A NUMBER - '))
+
+if n>10:
+    print('HelloWorld')
+    print('Welcome to pytho turorials')
+    print('We are learning Decision Making')
+    print('IF STATEMENT EXECUTED....')
+print('GOOD BYE')
